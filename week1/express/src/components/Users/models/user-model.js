8c39177b5ef3f423882dc064f09029575457042a
Volodymyr <vladimirboh1811@gmail.com>
2022-12-19 12:04:02 +0200

    week1.3 removed unique: false add pre(save)

--- week1/express/src/components/Users/models/user-model.js
+++ week1/express/src/components/Users/models/user-model.js
@@ -2,9 +2,15 @@ const { Schema, model } = require('mongoose');
 
 const UserSchema = new Schema({
     email: { type: String, unique: true, required: true },
-    firstName: { type: String, unique: false, required: true },
-    lastName: { type: String, unique: false, required: false },
-    password: { type: String, unique: false, required: true },
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: false },
+    password: { type: String, required: true },
 });
-
+UserSchema.pre('save', async function(next) {
+    if(this.password && this.isModified('password')) {
+        let salt = await bcrypt.genSaltSync(10)
+        this.password = await bcrypt.hash(this.password, salt)
+    }
+    next()
+})
 module.exports = model('User', UserSchema);
